bc26cde0ea5b89c86da68c7cec5f8ac960b0e06c
eddyionescu <eddyionescu@gmail.com>
2018-09-16 10:38:40 -0400

    change api from form to args as to make it easily consumable

--- gooseroute.py
+++ gooseroute.py
@@ -11,16 +11,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def gooseroute():
-    print(request.form)
-    start_lat = request.form['startLat']
-    start_lon = request.form['startLon']
-    end_lat = request.form['endLat']
-    end_lon = request.form['endLon']
+    print(request.args)
+    start_lat = request.args['startLat']
+    start_lon = request.args['startLon']
+    end_lat = request.args['endLat']
+    end_lon = request.args['endLon']
     # additonal optional options
-    walk_speed = request.form.get('walkSpeed', None) # in m/s
+    walk_speed = request.args.get('walkSpeed', None) # in m/s
     # includes all TNCs (uber/lyft), does include non-car uber-owned stuff,
     # like JUMP bike - TODO|eddy implement
-    # no_uber = request.get('noUber', None)
+    # no_uber = request.args.get('noUber', None)
     summaries = make_routing_call(
         start_lat=start_lat,
         start_lon=start_lon,
@@ -42,7 +42,7 @@ def make_routing_call(
     walk_speed=None,
 ):
     # TODO|eddy - allow choosing a departure range from the client-side
-    from_time = datetime.datetime.now() - datetime.timedelta(hours=6)
+    from_time = datetime.datetime.now() # - datetime.timedelta(hours=6)
     to_time = from_time + datetime.timedelta(minutes=60)
     request_dict = {
         'fromLat': start_lat,
